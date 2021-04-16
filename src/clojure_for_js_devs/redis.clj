505db3619639b1b93ffc862b6ef12644c7e81683
Musa Barighzaai <musa@circleci.com>
2021-04-16 11:13:26 -0400

    upd

--- src/clojure_for_js_devs/redis.clj
+++ src/clojure_for_js_devs/redis.clj
@@ -14,6 +14,7 @@
       this
       (do
         (println "Starting Redis component")
+        (println "Redis connection URI" this)
         (assoc this :connection {:pool {} :spec {:uri uri}}))))
 
   (stop [this]
