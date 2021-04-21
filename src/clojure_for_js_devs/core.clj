873a6d8dd5510219b7fbbcdd6e10a9fe50793f92
Tyler Sullberg <tysullberg@gmail.com>
2021-04-21 16:55:44 -0700

    Add comments explaining what each function is doing. (#5)

--- src/clojure_for_js_devs/core.clj
+++ src/clojure_for_js_devs/core.clj
@@ -2,34 +2,41 @@
   (:gen-class
    :main true)
   (:require
-   [clojure.tools.logging :as log]
    [com.stuartsierra.component :as component]
    [clojure-for-js-devs.http :as http]
    [clojure-for-js-devs.redis :as redis]))
 
-(defn main-system []
+(defn main-system
+  "Creates map of component dependencies with implementation of Lifecycle protocol"
+  []
   (component/system-map
-   ; TODO move redis URI, http port, and IP address to config.edn file
-   ; TODO create function to get set and get config vars
    :redis (redis/new-redis "redis://redis:6379")
    :http-server (component/using
                  (http/new-server "0.0.0.0" 8105)
                  [:redis])))
 
 (defn start
+  "Start components of system in dependency order. Runs SystemMap implementation of Lifecycle protocol's 'start' function"
   [system]
   (try
     (component/start system)
-    (catch Exception ex
-      (log/error ex "Failed to start the system"))))
+    (catch Exception _ex
+      (println "Failed to start the system"))))
 
 (defn stop
+  "Stop components of system in dependency order. Runs SystemMap implementation of Lifecycle protocol's 'stop' function"
   [system]
   (component/stop system))
 
+; Create a variable called *system* that can only be defined once
+; ^:dynamic means that this variable can be rebound
 (defonce ^:dynamic *system* nil)
 
-(defn -main []
+(defn -main
+  "Entry point to the application."
+  []
   (let [system (start (main-system))]
+    ; dynamically rebind *system* to the newly created SystemMap
     (alter-var-root #'*system* (constantly system))
+    ; Create hook that stops the component system in a controlled manner before the JVM completely shuts down 
     (.addShutdownHook (Runtime/getRuntime) (Thread. (partial stop system)))))
